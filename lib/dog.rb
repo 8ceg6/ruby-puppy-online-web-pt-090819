bf0cc83e429d7dc3994c1296e04e717b110d1656
cedric e goddard <8ceg6@comcast.net>
2019-10-11 12:30:07 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,24 @@
-# Add your code here
+require 'pry'
+
+class Dog 
+attr_accessor :name
+@@all= []
+
+def initialize(name)
+    @name= name
+    @@all << self
+  end
+
+def self.all
+  @@all  
+end
+
+def self.clear_all
+  @@all.clear
+end 
+ def self.print_all
+    @@all.each do|k9| 
+    puts k9
+    end
+ end
+end
