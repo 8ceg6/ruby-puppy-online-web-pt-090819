2e3fa82471728f89cf5993e85aab538f1d156ffb
cedric e goddard <8ceg6@comcast.net>
2019-10-11 13:53:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,7 +7,7 @@ attr_accessor :name, :save
 def initialize(name)
     @name= name
   @@all<< self
-    @save= save
+    @save= save.self
   end
 
 def self.all
@@ -17,6 +17,7 @@ end
 def self.clear_all
   @@all.clear
 end 
+ 
  def self.print_all
     @@all.each {|k9| puts k9.name} 
   end
